a8823f81ae38722c0013ad12a58a992bff1f1125
Priya Lorha <priya.lorha@gmail.com>
2021-01-16 01:16:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+        <a href ='/'> No No</a>
+        </header>
+        <main></main>
+        <footer>Only for demo purpose <br/> contact priya.lorha@gmail.com...</footer>
+
+      </div>
+    )
+  }
+}
