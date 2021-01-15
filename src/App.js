ffb6dfd8f943acf864a59942618a13b1c23ec5f6
Priya Lorha <priya.lorha@gmail.com>
2021-01-16 02:54:31 +0530

    products screen git push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ProductList from './componets/productList';
+import data from './data.json';
 
 export default class App extends Component {
+  constructor()
+  {
+    super();
+    this.state = {
+      products: data.products,
+      sort : "",
+      category:""
+    }
+  }
   render() {
+    console.log(this.state.products)
     return (
       <div className="grid-container">
-        <header>
+        <header className="brand">
         <a href ='/'> No No</a>
         </header>
-        <main></main>
+        <main>
+
+         
+        <div className="content">
+
+          <div className ="main">
+            <ProductList products={this.state.products}/></div>
+          <div className="sidebar"></div>
+        </div>
+
+        </main>
         <footer>Only for demo purpose <br/> contact priya.lorha@gmail.com...</footer>
 
       </div>
